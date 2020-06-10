5636b571360eeee674d9fd7bf8d2125cf3169970
elnathanau1 <elnathanau1@gmail.com>
2020-06-10 18:25:31 -0500

    keep an eye out for whether retry is working

--- download.py
+++ download.py
@@ -1,12 +1,13 @@
-import requests
-import json
 import concurrent.futures
-from retrying import retry
-from clint.textui import progress
-from pathlib import Path
-from os import path as ospath
 import csv
+import json
 import sys
+from os import path as ospath
+from pathlib import Path
+
+import requests
+from clint.textui import progress
+from retrying import retry
 
 SEASON_ENDPOINT = "https://aultima-api-flask.herokuapp.com/get/season/episodes"
 EPISODE_ENDPOINT = "https://aultima-api-flask.herokuapp.com/get/episode"
@@ -15,13 +16,16 @@ DOWNLOAD_ROOT = "/Users/eau/Documents/Development/aultima-api-flask/temp/"
 
 @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=4000)
 def get_download_link(url):
-    print("Retrieving download link for %s" % url)
-    new_parameters = {
-        "url": url
-    }
-    episode_json = requests.post(EPISODE_ENDPOINT, json=new_parameters).content.decode()
-    download_link = json.loads(episode_json)['download_link']
-    return download_link
+    try:
+        print("Retrieving download link for %s" % url)
+        new_parameters = {
+            "url": url
+        }
+        episode_json = requests.post(EPISODE_ENDPOINT, json=new_parameters).content.decode()
+        download_link = json.loads(episode_json)['download_link']
+        return download_link
+    except:
+        print("Failed to get download link for %s" % url)
 
 
 def download_show(show_name, season, aultima_show_url):
